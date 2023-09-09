01f1265560ee3af5150a42fb03a5219510aaa19f
mgodbole7 <meghnagodbole1@gmail.com>
2023-09-09 18:30:11 -0400

    Added qlen field to blast command. Removed perc_identity 100 from blast command

--- find_homologs.sh
+++ find_homologs.sh
@@ -5,7 +5,7 @@ queryfile="$1"
 subjectfile="$2"
 outputfile="$3"
 
-blastn -query "$queryfile" -subject "$subjectfile" -task blastn-short -outfmt "6 std sseq" -perc_identity 100 -out "$outputfile"
+blastn -query "$queryfile" -subject "$subjectfile" -task blastn-short -outfmt "6 std sseq qlen" -out "$outputfile"
 
 # get length of query sequence
 query_length=$(awk '/^>/ {next} {seq = seq $0} END {print length(seq)}' "$queryfile")
@@ -17,3 +17,7 @@ awk -F"\t" '{if ($4 == '"$query_length"') print}' "$outputfile" > "${outputfile}
 perfectmatch_count=$(cat "${outputfile}_filtered_samelength" | wc -l)
 
 echo "Number of perfect matches: $perfectmatch_count"
+
+
+# added qlen field to blastn command
+# removed -perc_identity 100
