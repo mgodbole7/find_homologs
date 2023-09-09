7becb2b02e8e72e2085b9cb21afdd33784556616
mgodbole7 <meghnagodbole1@gmail.com>
2023-09-09 15:54:52 -0400

    Add find_perfect_matches.sh script

---
+++ find_perfect_matches.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# usage: ./find_perfect_matches.sh <query file> <subject file> <output file>
+
+queryfile="$1"
+subjectfile="$2"
+outputfile="$3"
+
+blastn -query "$queryfile" -subject "$subjectfile" -task blastn-short -outfmt "6 std sseq" -perc_identity 100 -out "$outputfile"
+
+# get length of query sequence
+query_length=$(awk '/^>/ {next} {seq = seq $0} END {print length(seq)}' "$queryfile")
+
+# filter results to same length as query length and write to another file
+awk -F"\t" '{if ($4 == '"$query_length"') print}' "$outputfile" > "${outputfile}_filtered_samelength"
+
+# count number of perfect matches
+perfectmatch_count=$(cat "${outputfile}_filtered_samelength" | wc -l)
+
+echo "Number of perfect matches: $perfectmatch_count"
