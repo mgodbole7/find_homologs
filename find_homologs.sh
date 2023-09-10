0e7901c7176f68cb906a3824fec96611c6831aba
mgodbole7 <meghnagodbole1@gmail.com>
2023-09-09 20:18:17 -0400

    changed name in top comment. removed task option. removed perc identity in blast command. removed calculating length of query seq as there are multiple query seqs in query file. filtered for identity and match length in awk command

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# usage: ./find_perfect_matches.sh <query file> <subject file> <output file>
+# usage: ./find_homologs.sh <query file> <subject file> <output file>
 
 queryfile="$1"
 subjectfile="$2"
@@ -7,14 +7,8 @@ outputfile="$3"
 
 tblastn -query "$queryfile" -subject "$subjectfile" -outfmt "6 std sseq qlen" -out "$outputfile"
 
-# get length of query sequence
-query_length=$(awk '/^>/ {next} {seq = seq $0} END {print length(seq)}' "$queryfile")
-
-# calculate 90% of the query length
-ninety_perc_query_length=$((query_length * 90 / 100))
-
 # filter results to > 90% query length and > 30% identity
-awk -F"\t" '{if ($4 > '"$ninety_perc_query_length"' && $3 > 30.000) print}' "$outputfile" > "${outputfile}_filtered"
+awk -F"\t" '{if ($4 > (0.9 * $14) && ($3 > 30.000)) print}' "$outputfile" > "${outputfile}_filtered"
 
 # count number of perfect matches
 match_count=$(cat "${outputfile}_filtered" | wc -l)
@@ -31,4 +25,9 @@ echo "Number of matches: $match_count"
 # awk filter - alignment length must be > 90% query length
 # awk filter - only > 30.000 seq identity
 
-# what to do for task??
+# removed task option
+
+# changed name in comment at top
+
+# remove percidentty in blast command
+# removed calculating length of query seq in query file as there are multiple query sequences
