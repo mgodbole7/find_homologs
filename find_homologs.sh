c021df9d8411677a72c92a3b3c197fbc02ce8c4e
mgodbole7 <meghnagodbole1@gmail.com>
2023-09-09 20:28:18 -0400

    Removed comments at bottom

--- find_homologs.sh
+++ find_homologs.sh
@@ -14,20 +14,3 @@ awk -F"\t" '{if ($4 > (0.9 * $14) && ($3 > 30.000)) print}' "$outputfile" > "${o
 match_count=$(cat "${outputfile}_filtered" | wc -l)
 
 echo "Number of matches: $match_count"
-
-
-# added qlen field to blastn command
-# removed -perc_identity 100
-
-# changed blastn to tblastn
-
-# calculated 90% of query length and stored as variable
-# awk filter - alignment length must be > 90% query length
-# awk filter - only > 30.000 seq identity
-
-# removed task option
-
-# changed name in comment at top
-
-# remove percidentty in blast command
-# removed calculating length of query seq in query file as there are multiple query sequences
